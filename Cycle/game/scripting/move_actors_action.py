46b102c2e9206560eba879391d502c7a38150435
antmorera <ant.morera.lb@gmail.com>
2022-11-11 17:12:00 -0700

    made changes in scripting, casting and main.

--- Cycle/game/scripting/move_actors_action.py
+++ Cycle/game/scripting/move_actors_action.py
@@ -1,7 +1,8 @@
 from game.scripting.action import Action
+import constants
 
 
-# TODO: Implement MoveActorsAction class here! 
+# TODO: Implement MoveActorsAction class here!
 
 # Override the execute(cast, script) method as follows:
 # 1) get all the actors from the cast
@@ -13,11 +14,27 @@ class MoveActorsAction(Action):
     also will override the method execute from Action
     Class.
     """
+
     def __init__(self) -> None:
         super().__init__()
 
-
     def execute(self, cast, script):
-        all_actors = cast.get_all_actors()
-        for actor in all_actors:
+
+        actors = cast.get_all_actors()
+        for actor in actors:
             actor.move_next()
+
+        player1 = cast.get_first_actor("snake")
+        player2 = cast.get_second_actor("snake2")
+
+        if script.get_actions("update")[1]._is_game_over:
+            player1.grow_tail(1, constants.WHITE)
+            player2.grow_tail(1, constants.WHITE)
+
+        else:
+
+            player1 = cast.get_first_actor("snake")
+            player2 = cast.get_second_actor("snake2")
+
+            player1.grow_tail(1, constants.RED)
+            player2.grow_tail(1, constants.GREEN)
