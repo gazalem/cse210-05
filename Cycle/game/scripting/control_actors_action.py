9f0b73e353b34630fd4289339e386ac0d32f3c91
MONTOYA, Alan <mon21033@byui.edu>
2022-11-14 22:11:08 -0500

    Two clycles and their controls are working

--- Cycle/game/scripting/control_actors_action.py
+++ Cycle/game/scripting/control_actors_action.py
@@ -20,7 +20,8 @@ class ControlActorsAction(Action):
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
         self._keyboard_service = keyboard_service
-        self._direction = Point(constants.CELL_SIZE, 0)
+        self._player_one_direction = Point(0, -constants.CELL_SIZE)
+        self._player_two_direction = Point(0, -constants.CELL_SIZE)
 
     def execute(self, cast, script):
         """Executes the control actors action.
@@ -33,40 +34,40 @@ class ControlActorsAction(Action):
 
         # left
         if self._keyboard_service.is_key_down('a'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
+            self._player_one_direction = Point(-constants.CELL_SIZE, 0)
         
         # right
         if self._keyboard_service.is_key_down('d'):
-            self._direction = Point(constants.CELL_SIZE, 0)
+            self._player_one_direction = Point(constants.CELL_SIZE, 0)
         
         # up
         if self._keyboard_service.is_key_down('w'):
-            self._direction = Point(0, -constants.CELL_SIZE)
+            self._player_one_direction = Point(0, -constants.CELL_SIZE)
         
         # down
         if self._keyboard_service.is_key_down('s'):
-            self._direction = Point(0, constants.CELL_SIZE)
+            self._player_one_direction = Point(0, constants.CELL_SIZE)
         
         player_one = cast.get_first_actor("player_one")
-        player_one.turn_head(self._direction)
+        player_one.turn_head(self._player_one_direction)
 
         # Player Two - controls
 
         # left
         if self._keyboard_service.is_key_down('j'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
+            self._player_two_direction = Point(-constants.CELL_SIZE, 0)
         
         # right
         if self._keyboard_service.is_key_down('l'):
-            self._direction = Point(constants.CELL_SIZE, 0)
+            self._player_two_direction = Point(constants.CELL_SIZE, 0)
         
         # up
         if self._keyboard_service.is_key_down('i'):
-            self._direction = Point(0, -constants.CELL_SIZE)
+            self._player_two_direction = Point(0, -constants.CELL_SIZE)
         
         # down
         if self._keyboard_service.is_key_down('k'):
-            self._direction = Point(0, constants.CELL_SIZE)
+            self._player_two_direction = Point(0, constants.CELL_SIZE)
         
-        # player_two = cast.get_first_actor("player_two")
-        # player_two.turn_head(self._direction)
+        player_two = cast.get_first_actor("player_two")
+        player_two.turn_head(self._player_two_direction)
